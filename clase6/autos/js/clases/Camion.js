f1f274061f073160851df8e275b73fa83b50fa1c
Alejandro Krumkamp <alekd9@gmail.com>
2017-05-19 01:53:30 -0300

    agrego metodos como corresponde

--- clase6/autos/js/clases/Camion.js
+++ clase6/autos/js/clases/Camion.js
@@ -2,11 +2,19 @@ function Camion(maximoCarga,patente,peso){
 	Vehiculo.call(this, patente, peso);
 	this.autosCargados = [];
 	this.maximoCarga = maximoCarga;
-	this.pesoTransportado = function(){
+
+	this.pesoTransportado = ()=> {
 		var pesoTransportado = 0;
 		for(i in this.autosCargados){
 			pesoTransportado += this.autosCargados[i].peso;
 		}
 		return pesoTransportado;
 	}
+	this.cargarAuto = (unAuto) => {
+		if(unAuto.peso < this.maximoCarga)
+			this.autosCargados.push(unAuto);
+		else
+			console.log("No se pudo cargar el auto " + unAuto.patente + " al camion" + this.patente);
+	}
+	
 }
--- clase6/autos/js/index.js
+++ clase6/autos/js/index.js
@@ -2,17 +2,9 @@ function numeroRandomHasta(maximo){
 	return Math.round(Math.random()*maximo);
 }
 
-
-function cargarAuto(unAuto,unCamion){
-	if(unAuto.peso < unCamion.maximoCarga)
-		unCamion.autosCargados.push(unAuto);
-	else
-		console.log("No se pudo cargar el auto " + unAuto.patente + " al camion" + unCamion.patente);
-}
-
 var unAuto = new Auto("AAA999",numeroRandomHasta(900),2017);
 var otroAuto = new Auto("AAA997",numeroRandomHasta(900),2017);
 var unCamion = new Camion(900,"AAA998",1200);
 
-cargarAuto(unAuto,unCamion);
-cargarAuto(otroAuto,unCamion);
+unCamion.cargarAuto(unAuto);
+unCamion.cargarAuto(otroAuto);
