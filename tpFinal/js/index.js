893eff257002179e6fb3e8dfe63a57a0655d7661
Alejandro Krumkamp <alekd9@gmail.com>
2017-07-06 20:40:28 -0300

    buscador basico funcionando

--- tpFinal/js/index.js
+++ tpFinal/js/index.js
@@ -11,11 +11,16 @@ var init = function(){
 
 function showArtists(data){
     var artists = data.artists.items;
+    var artistsFoundUl = $("<ul></ul>").attr("id","artistsFoundUl").text("Results:");
+    $("#container").append(artistsFoundUl);
     for(i in artists){
         console.log(artists[i].name);
+        var artistFoundLi = $("<li></li>").addClass("artist").text(artists[i].name);
         if(artists[i].images[0] != undefined){
-            var artistImage = artists[i].images[0].url;
-            console.log(artistImage);
+            var artistImageUrl = artists[i].images[0].url;
+            $(artistFoundLi).html("<img src='"+artistImageUrl+"' width='100' height='100' >");
+            console.log(artistImageUrl);
         }
+        $("#artistsFoundUl").append(artistFoundLi);
     }
 }
