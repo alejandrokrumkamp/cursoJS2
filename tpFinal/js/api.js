004b2e504ad61b2dbcc8b204106bab1739fb368f
Alejandro Krumkamp <alekd9@gmail.com>
2017-07-06 20:58:41 -0300

    agrego mas cosas

--- tpFinal/js/api.js
+++ tpFinal/js/api.js
@@ -1,9 +1,11 @@
 var searchArtist= function(anArtist,callback) {
+    console.log("Fetching data...");
     $.ajax({
         type:"POST",
         dataType: "json",
         url: 'https://platzi-music-api.now.sh/search?q='+anArtist+'&type=artist',
         success: function(data) {
+            console.log("Fetching data finished.")
             callback(data);
         },
         error: function(data) {
--- tpFinal/js/index.js
+++ tpFinal/js/index.js
@@ -8,18 +8,18 @@ var init = function(){
 
 }
 
-
 function showArtists(data){
     var artists = data.artists.items;
-    var artistsFoundUl = $("<ul></ul>").attr("id","artistsFoundUl").text("Results:");
+    if($("#artistsFoundUl") != undefined){
+        $("#artistsFoundUl").empty();
+    }
+    var artistsFoundUl = $("<ul></ul>").attr("id","artistsFoundUl");
     $("#container").append(artistsFoundUl);
     for(i in artists){
-        console.log(artists[i].name);
         var artistFoundLi = $("<li></li>").addClass("artist").text(artists[i].name);
         if(artists[i].images[0] != undefined){
             var artistImageUrl = artists[i].images[0].url;
-            $(artistFoundLi).html("<img src='"+artistImageUrl+"' width='100' height='100' >");
-            console.log(artistImageUrl);
+            $(artistFoundLi).html("<p>"+artists[i].name+"</p><img src='"+artistImageUrl+"' width='100' height='100' >");
         }
         $("#artistsFoundUl").append(artistFoundLi);
     }
